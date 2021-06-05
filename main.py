d39ffa3eaa5fb0e534cebd0d15b9d6aeeaceaae2
DulgheruVitalii20 <85393133+DulgheruVitalii20@users.noreply.github.com>
2021-06-05 14:35:22 +0300

    Add files via upload
    emoji and antigravity

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-a=int(input("introdu primul numar:"))
-b=int(input("introdu al doilea numar:"))
-c=int(input("introdu al treilea numar:"))
-d=[]
-d.append(a)
-d.append(b)
-d.append(c)
-for i in range(0,3):
-    for j in range(0, 3):
-        for k in range(0, 3):
-            if(i!=j&j!=k&k!=i):
-                print(d[i],d[j],d[k])
+# '''https://www.webfx.com/tools/emoji-cheat-sheet/'''
+# import emoji
+#
+# print(emoji.emojize('Impact is :thumbsup::thumbsdown:', use_aliases=True))
+# print(emoji.emojize('Impact is :wink:,:wink:', use_aliases=True))
+# print(emoji.emojize('Students are :blush: ', use_aliases=True))
+# print(emoji.demojize('Students are'))
+
+import antigravity
